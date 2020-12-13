a7cfa5bcf5a0225fbf6c09dbc3125de7347769bb
craig.ian@itsligo.ie <craig.ian@itsligo.ie>
2020-12-13 23:36:00 +0000

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,41 @@
+ghdl -s -fsynopsys arithmetic.vhdl
+ghdl -s comparison4bit.vhdl
+ghdl -s hexToSevenSegment.vhdl
+ghdl -s hexToSevenSegment_tb.vhdl
+ghdl -s logical.vhdl
+ghdl -s magic.vhdl
+ghdl -s magic2.vhdl
+ghdl -s mux10_4.vhdl
+ghdl -s mux_10_4_tb.vhdl
+ghdl -s or_4bit.vhdl
+ghdl -s or_4bit_tb.vhdl
+ghdl -s -fsynopsys computer_4bit.vhdl
+ghdl -s -fsynopsys computer_4bit_tb1.vhdl
+
+ghdl -a -fsynopsys arithmetic.vhdl
+ghdl -a comparison4bit.vhdl
+ghdl -a hexToSevenSegment.vhdl
+ghdl -a hexToSevenSegment_tb.vhdl
+ghdl -a logical.vhdl
+ghdl -a magic.vhdl
+ghdl -a magic2.vhdl
+ghdl -a mux10_4.vhdl
+ghdl -a mux_10_4_tb.vhdl
+ghdl -a or_4bit.vhdl
+ghdl -a or_4bit_tb.vhdl
+ghdl -a -fsynopsys computer_4bit.vhdl
+ghdl -a -fsynopsys computer_4bit_tb1.vhdl
+
+ghdl -e -fsynopsys arithmetic
+ghdl -e comparison4bit
+ghdl -e hexToSevenSegment
+ghdl -e hexToSevenSegment_tb
+ghdl -e logical
+ghdl -e magic
+ghdl -e magic2clk
+ghdl -e mux10_4
+ghdl -e mux_10_4_tb
+ghdl -e orGate
+ghdl -e or_4bit_tb
+ghdl -e -fsynopsys computer_4bit
+ghdl -e -fsynopsys computer_4bit_tb1
